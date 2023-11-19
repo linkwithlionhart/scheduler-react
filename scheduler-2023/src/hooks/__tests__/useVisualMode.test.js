4d94105de50d3606501f481dc4e977bb34c0e8d3
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-18 16:35:03 -0800

    Build a hook: useVisualMode

--- scheduler-2023/src/hooks/__tests__/useVisualMode.test.js
+++ scheduler-2023/src/hooks/__tests__/useVisualMode.test.js
@@ -1,2 +1,14 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 import useVisualMode from "hooks/useVisualMode";
+
+const FIRST = "FIRST";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+// export default function useVisualMode(initial) {
+//   return { mode: initial };
+// }
