83ceb8340b455aafadfcc61d137aba1b27ec6a98
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-20 00:44:00 -0800

    Add back and implement limits

--- scheduler-2023/src/hooks/useVisualMode.js
+++ scheduler-2023/src/hooks/useVisualMode.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [ mode, setMode ] = useState(initial);
+  // const [ mode, setMode ] = useState(initial);
+  const [ history, setHistory ] = useState([initial]);
   
-  function transition(newMode) {
-    setMode(newMode);
+  function transition(newMode, replace = false) {
+    // setMode(newMode);
+    setHistory(prev => (replace ? [...prev.slice(0, prev.length - 1), newMode] : [...prev, newMode]));
+  }
+
+  function back() {
+    // setMode(history[history.length -1]);
+    setHistory(prev => (prev.length > 1 ? [...prev.slice(0, prev.length - 1)] : prev));
   }
   
-  return { mode, transition };
+  return { mode: history[history.length -1], transition, back };
 }
