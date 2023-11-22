449a75506da38e7ba1e898377ad16f6a7578871b
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-21 23:16:11 -0800

    Perform integration testing and debugging testS

--- scheduler-2023/src/components/__tests__/Application.test.js
+++ scheduler-2023/src/components/__tests__/Application.test.js
@@ -1,22 +1,75 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
-
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
-import { render } from "@testing-library/react";
-
-/*
-  We import the component that we are testing
-*/
 import Application from "components/Application";
+import {
+  fireEvent,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+  getByText,
+  prettyDOM,
+  queryByText,
+  render,
+  waitForElement,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 
-/*
-  A test that renders a React Component
-*/
-it("renders without crashing", () => {
-  render(<Application />);
+
+
+describe("Application", () => {
+  
+  it("loads data, books an interview and reduces the sports remaining for the first day by 1", async() => {
+    const { container, debug } = render(<Application />);
+    
+    // Wait for initial data to load.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    // Find the first appointment.
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
+    // Click the "Add" button to create a new appointment.
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    // Enter the student name.
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value : "Lydia Miller-Jones"}
+    });
+
+    // Select an interviewer.
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    // Click the "Save" button.
+    fireEvent.click(getByText(appointment, "Save"));
+  
+    // console.log(prettyDOM(container));
+    // console.log(prettyDOM(appointments)); 
+    // console.log(prettyDOM(appointment));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    // expect(getByText(appointment, "Saving")).not.toBeInTheDocument();
+
+    // Wait for the "Saving" status to be removed.
+    await waitForElementToBeRemoved(() => getByText(appointment, "Saving"));
+    
+    // Wait for the student's name to be displayed.
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    // Find the day node that contains the text "Monday".
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+    
+    // Check the day has the text "no spots remaining".
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+  });
+  
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
 });
+
