9fc2f4ef395ccb50daa41a6acc2e236b3404b2dc
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-24 14:47:01 -0800

    Write cypress tests to navigate days in component

--- scheduler-2023/cypress/integration/navigation.spec.js
+++ scheduler-2023/cypress/integration/navigation.spec.js
@@ -1,5 +1,24 @@
 describe("Navigation", () => {
+  
   it("should visit root", () => {
     cy.visit("/");
   });
-});
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+
+  it("should find the list item for the correct day and click it", () => {
+    cy.get("li").contains("Tuesday").click();
+  });
+
+  it("should find the list item for the correct day, click it, and contain the correct color", () => {
+    cy.contains("li", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+  });
+
+  });
